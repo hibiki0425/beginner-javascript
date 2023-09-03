d7037335a2ebf3a556561825db988408d44b2f7a
hibiki0425 <pizza.hibiki@gmail.com>
2023-09-03 20:54:06 +0800

    finish module #1

--- playground/types.js
+++ playground/types.js
@@ -1,26 +1,76 @@
-const singleQ = 'Peter';
-const doubleQ = "double";
-const backticks = `oh`;
+/* String */
 
-const sentense = "she's so \"cool\"";
-const sentense2 = `she's so "cool"!`;
+// const singleQ = 'Peter';
+// const doubleQ = "double";
+// const backticks = `oh`;
 
-const song = `This is 
+// const sentence = "she's so \"cool\"";
+// const sentence2 = `she's so "cool"!`;
 
-my 
+// const song = `This is 
 
-very first
+// my 
+
+// very first
 
 
-Song`;
+// Song`;
 
-const hello = `Hello, my name is ${singleQ}. Nice to meet you. I am ${50 +5} years old.`;
+// const hello = `Hello, my name is ${singleQ}. Nice to meet you. I am ${50 +5} years old.`;
 
-const html = `
-    <div>
-        <h2>${singleQ}</h2>
-        <p>${hello}</p>
-    </div>
-`;
+// const html = `
+//     <div>
+//         <h2>${singleQ}</h2>
+//         <p>${hello}</p>
+//     </div>
+// `;
 
-document.body.innerHTML = html;
+// document.body.innerHTML = html;
+
+/* numbers */
+
+// const smarties = 20;
+// const kids = 3;
+// const eachKidGets = Math.floor(smarties / kids);
+// const dadGets = smarties % kids;
+
+// console.log(`each kid gets ${eachKidGets}`);
+
+// const power = 25 ** 2;
+// console.log(power);
+// typeof Infinity and -Infinity is number
+// NaN: Not a number, also typeof NaN is number
+
+/* objects: collection of functionality */
+// const person = {
+//     first: 'Peter',
+//     last: 'Wong',
+//     age: 50
+// };
+
+/* null and undefined */
+// let dog;
+// console.log(dog); // undefined
+
+// // null is a variable of nothing
+// const cher = {
+//     first: 'cher'
+// }; // last name will be undefined
+
+// const teller = {
+//     first: 'Raymond',
+//     last: 'Teller'
+// };
+
+// teller.first = 'Teller';
+// teller.last = null;
+
+/* boolean and Equality */
+let isDrowning = false;
+const age = 18;
+const ofAge = age > 19;
+console.log(ofAge);
+
+// === will check both side of the values and the type are the same
+console.log(10 == '10');  // true
+console.log(10 === '10'); // false
